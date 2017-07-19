c8758a844dd38417705532a08d4ee09b02bca21f
TESTDEV\SoftDev <victorreis@me.com>
2017-07-19 18:56:22 -0300

    Revert "AEho"
    This reverts commit 07d039f90ebafdb7d673c9c6c1e39b45c0c9aa52.

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,48 @@
-TO FALANDO, NAO TO FALANDO? ENTAO MORRE DIABO
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'APP KEY MANAGER' });
+});
+
+router.get('/releaseappkey', function(req, res){
+  res.render('releaseappkey', {
+    title: 'Release App Key'
+  });
+});
+
+router.get('/deleteappkey', function(req, res){
+  res.render('deleteappkey', {
+    title: 'Delete App Key'
+  });
+});
+
+router.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+router.get('/contact', function(req, res){
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+
+//TESTE DE BOTAO E TEXT AREA releaseappkey
+router.post('/releaseappkey', function (req, res) {
+    console.log(req.body.email);
+    console.log(req.body.keyquantity);
+    res.send('Hora do Palmeira Brincar?');
+});
+
+//TESTE DE BOTAO E TEXT AREA deleteappkey
+router.post('/', function (req, res) {
+    console.log(req.body.emaildelete);
+    console.log(req.body.keycodedelete);
+    res.send('Hora do Palmeira Brincar?');
+});
+
+module.exports = router;
